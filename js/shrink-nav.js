bf405e2b9b1dc29b57c0a25f5fb386ba8d1e8caf
shubhom <shbhomsrivastav@gmail.com>
2022-10-07 20:48:03 +0530

    resize on width shrink

--- js/shrink-nav.js
+++ js/shrink-nav.js
@@ -1,10 +1,27 @@
-window.onscroll = function () {
+window.onscroll = function() {
+  resizeFunction();
   scrollFunction();
 };
+
+window.onresize = function() {
+  resizeFunction();
+}
+
+function resizeFunction() {
+  if (screen.width <= 780) {
+    document.getElementById("header").style.height = "4rem";
+    document.getElementById("logo").style.fontSize = "24px";
+  }
+  else {
+    document.getElementById("header").style.height = "6rem";
+    document.getElementById("logo").style.fontSize = "2em";
+  }
+}
+
 function scrollFunction() {
   if (
     document.body.scrollTop > 900 ||
-    document.documentElement.scrollTop > 900
+    document.documentElement.scrollTop > 900 || screen.width <= 780
   ) {
     document.getElementById("header").style.height = "4rem";
     document.getElementById("logo").style.fontSize = "24px";
